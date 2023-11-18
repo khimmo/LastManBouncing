1e32123a9369e65d7911df9b6a063f97098a9c1a
Adil zain <adilzain009@gmail.com>
2023-11-19 02:12:14 +0400

    arena

--- Assets/Scripts/Zone/ZoneDestroy.cs
+++ Assets/Scripts/Zone/ZoneDestroy.cs
@@ -6,7 +6,9 @@ public class ZoneDestroy : MonoBehaviour
 {
     public ZoneShrink zoneShrink;
 
-    void OnCollisionEnter(Collision other)
+    public float destroyTimer;
+
+    /*void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("Layer 1"))
         {
@@ -21,6 +23,31 @@ public class ZoneDestroy : MonoBehaviour
         if (other.gameObject.CompareTag("Layer 3"))
         {
             Destroy(zoneShrink.zoneLayer3);
+        } 
+        
+        if (other.gameObject.CompareTag("Layer 4"))
+        {
+            Destroy(zoneShrink.zoneLayer4);
         }
+
+        if (other.gameObject.CompareTag("Layer 5"))
+        {
+            Destroy(zoneShrink.zoneLayer5);
+        }
+
+        if (other.gameObject.CompareTag("Layer 6"))
+        {
+            Destroy(zoneShrink.zoneLayer6);
+        }
+
+        if (other.gameObject.CompareTag("Layer 7"))
+        {
+            Destroy(zoneShrink.zoneLayer7);
+        }
+    }*/
+
+    public void DestroyTimer()
+    {
+        destroyTimer += Time.deltaTime;
     }
 }
--- Assets/Scripts/Zone/ZoneShrink.cs
+++ Assets/Scripts/Zone/ZoneShrink.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Linq;
 using System.Net.Sockets;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ZoneShrink : MonoBehaviour
 {
@@ -11,6 +13,7 @@ public class ZoneShrink : MonoBehaviour
     [SerializeField] private Rigidbody layer4rb;
     [SerializeField] private Rigidbody layer5rb;
     [SerializeField] private Rigidbody layer6rb;
+    [SerializeField] private Rigidbody layer7rb;
 
     [SerializeField] public GameObject zoneLayer1;
     [SerializeField] public GameObject zoneLayer2;
@@ -18,98 +21,124 @@ public class ZoneShrink : MonoBehaviour
     [SerializeField] public GameObject zoneLayer4;
     [SerializeField] public GameObject zoneLayer5;
     [SerializeField] public GameObject zoneLayer6;
+    [SerializeField] public GameObject zoneLayer7;
 
-    [SerializeField] private float timer ;
+    [SerializeField] private float timer = 25f;
 
     [SerializeField] private int zoneIndex = 0;
 
+    [SerializeField] private TextMeshProUGUI timerText;
+ 
     void Timer()
     {
-        timer += Time.deltaTime;
-        Mathf.Round(timer);
+        timer -= Time.deltaTime;
+        timerText.text = string.Format("Time until zone shrink: " + Mathf.Round(timer));
     }
 
     void ShrinkZone()
     {
-        if (timer >= 5 && zoneIndex == 0)
+        if (timer <= 0 && zoneIndex == 0)
         {
-            Layer1Drop();
-            timer = 0;
+            StartCoroutine(Layer1Drop());
+            timer = 25 ;
             zoneIndex++;
         }
 
-        if (timer >= 5 && zoneIndex == 1)
+        if (timer <= 0 && zoneIndex == 1)
         {
-            Layer2Drop();
-            timer = 0;
+            StartCoroutine(Layer2Drop());
+            timer = 25;
             zoneIndex++;
         }
 
-        if (timer >= 5 && zoneIndex == 2)
+        if (timer <= 0 && zoneIndex == 2)
         {
-            Layer3Drop();
-            timer = 0;
+            StartCoroutine(Layer3Drop());
+            timer = 25;
             zoneIndex++;
         }
 
-        if (timer >= 5 && zoneIndex == 3)
+        if (timer <= 0 && zoneIndex == 3)
         {
-            layer4Drop();
-            timer = 0;
-            zoneIndex++;
-
-        }
-
-        if (timer >= 5 && zoneIndex == 4)
-        {
-            Layer5Drop();
-            timer = 0;
+            StartCoroutine(layer4Drop());
+            timer = 25;
             zoneIndex++;
         }
 
-        if (timer >= 5 && zoneIndex == 5)
+        if (timer <= 0 && zoneIndex == 4)
         {
-            Layer6Drop();
-            timer = 0;
+            StartCoroutine(Layer5Drop());
+            timer = 25;
             zoneIndex++;
         }
 
+        if (timer <= 0 && zoneIndex == 5)
+        {
+            StartCoroutine(Layer6Drop());
+            timer = 25;
+            zoneIndex++;
+        }
+
+        if(timer <= 0 && zoneIndex == 6)
+        {
+            StartCoroutine(Layer7drop());
+            timer = 25;
+            zoneIndex++;
+        }
     }
-    void Layer1Drop()
+
+    IEnumerator Layer1Drop()
     {
         layer1rb.constraints = RigidbodyConstraints.None;
-        //transform.Translate(Vector3.down * Time.deltaTime);
+        yield return new WaitForSeconds(3);
+        Destroy(zoneLayer1);
     }
-    
-    void Layer2Drop()
+
+    IEnumerator Layer2Drop()
     {
         layer2rb.constraints = RigidbodyConstraints.None;
-    } 
-    
-    void Layer3Drop()
+        yield return new WaitForSeconds(3);
+        Destroy(zoneLayer2);
+    }
+
+    IEnumerator Layer3Drop()
     {
         layer3rb.constraints = RigidbodyConstraints.None;
+        yield return new WaitForSeconds(3);
+        Destroy(zoneLayer3);
     }
 
-    void layer4Drop()
+    IEnumerator layer4Drop()
     {
         layer4rb.constraints = RigidbodyConstraints.None;
+        yield return new WaitForSeconds(3);
+        Destroy(zoneLayer4);
     }
 
-    void Layer5Drop()
+    IEnumerator Layer5Drop()
     {
         layer5rb.constraints = RigidbodyConstraints.None;
+        yield return new WaitForSeconds(3);
+        Destroy(zoneLayer5);
     }
 
-    void Layer6Drop()
+    IEnumerator Layer6Drop()
     {
         layer6rb.constraints = RigidbodyConstraints.None;
+        yield return new WaitForSeconds(3);
+        Destroy(zoneLayer6);
     }
-        
+
+    IEnumerator Layer7drop()
+    {
+        layer7rb.constraints = RigidbodyConstraints.None;
+        yield return new WaitForSeconds(3);
+        Destroy(zoneLayer7);
+    }
 
     void Update()
     {
-        ShrinkZone();
         Timer();
+        ShrinkZone();
     }
 }
