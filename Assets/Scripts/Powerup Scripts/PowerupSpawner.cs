d1664ac90a5f67b3dc19363e9ad5c1d91ebb4b91
Khalid Himmo <khalidhimmo07@gmail.com>
2023-12-13 10:29:17 +0400

    Added random powerup spawner in final zone

---
+++ Assets/Scripts/Powerup Scripts/PowerupSpawner.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupSpawner : MonoBehaviour
+
+{
+    public GameObject[] powerupPrefabs; // Array of powerup prefabs to spawn
+    public float spawnRadius = 30f;     // Radius within which powerups will spawn
+    public float spawnInterval = 7.5f;  // Time interval between spawns
+
+    public float spawnHeight;
+    public GameObject sizePowerupPrefab;
+    public GameObject speedPowerupPrefab;
+    public GameObject confusionPowerupPrefab;
+    public GameObject shockwavePowerupPrefab;
+
+    public void beginSpawning()
+    {
+        // Start spawning coroutine
+        StartCoroutine(SpawnPowerups());
+        Debug.Log("Beginning Spawning");
+    }
+
+    IEnumerator SpawnPowerups()
+    {
+        while (true)
+        {
+            
+            Vector2 randomPosition = Random.insideUnitCircle * spawnRadius;
+            Vector3 spawnPosition = new Vector3(randomPosition.x, spawnHeight, randomPosition.y) + transform.position;
+
+            
+            int randomNumber = Random.Range(1, 5);
+            
+
+            
+            switch (randomNumber)
+            {
+                case 1:
+                    Instantiate(sizePowerupPrefab, spawnPosition, Quaternion.identity);
+                    Debug.Log("Spawned " + randomNumber);
+                    break;
+                case 2:
+                    Instantiate(speedPowerupPrefab, spawnPosition, Quaternion.identity);
+                    Debug.Log("Spawned " + randomNumber);
+                    break;
+                case 3:
+                    Instantiate(confusionPowerupPrefab, spawnPosition, Quaternion.identity);
+                    Debug.Log("Spawned " + randomNumber);
+                    break;
+                case 4:
+                    Instantiate(shockwavePowerupPrefab, spawnPosition, Quaternion.identity);
+                    Debug.Log("Spawned " + randomNumber);
+                    break;
+                default:
+                    Debug.Log("Invalid # for powerup instantiation.");
+                    break;
+            }
+
+            // Wait for the next spawnInterval
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+}
